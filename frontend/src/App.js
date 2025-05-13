fccdfe9ba54550ac5236065313a23f818f3044f7
Suds Kumar <suds123@gmail.com>
2025-05-13 22:30:48 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 // frontend/src/App.js
 import React from 'react';
-import TradeSageInterface from './components/TradeSageInterface';
+import TradeSageInterface from './components/TradingDashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TradeSageInterface />
+      <TradingDashboard />
     </div>
   );
 }
