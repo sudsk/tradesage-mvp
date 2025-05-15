04a8ccbda4e7958b9c41542ddf241b7dd4d729fe
Suds Kumar <suds123@gmail.com>
2025-05-15 23:54:48 +0100

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,5 +2,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // Add cssnano for production builds
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
 }
